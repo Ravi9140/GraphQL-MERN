ee3ae492e436063e3152f47515ad8378276b0967
Ravindra Patel <ravindra.patel@bluepineapple.io>
2022-12-30 11:48:20 +0530

    Graphql Project

---
+++ readme.txt
@@ -0,0 +1,12 @@
+//1. Go to Mongo Atlas and Login
+//2. Go to Network Access and Add Current IP Address in IP Addresses
+//3. Click on Connect button on Cluster0 in MERN_Grapql Database
+//4. Click on connect to MonogDBCompass option
+//5. Copy the Connection String and Paste in new connection option in MongoDBCompass App
+//6. Replace the <password> with ravi@991998 in connection string
+//7. Replace the test with clients_projects in connection string
+
+//Command to run the Project
+npm run dev //in MERN_GRAPHQL folder
+
+//localhost:5000/graphql in browser to check graphql queries
---
+++ server/schema/schema.js
@@ -0,0 +1,78 @@
+// const { projects, clients } = require("../sampleData.js");
+
+//Monogoose Models
+const Project = require("../models/Project");
+const Client = require("../models/Client");
+
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+} = require("graphql");
+
+//Client Type
+const ClientType = new GraphQLObjectType({
+  name: "Client",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+//Project Type
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    //Defining the relations between the database models in the graphql syntax
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return Client.findById(parent.clientId);
+      },
+    },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return Client.find();
+      },
+    },
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Client.findById(args.id);
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Project.findById(args.id);
+      },
+    },
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return Project.find();
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
